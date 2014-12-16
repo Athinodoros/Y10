f244795d8343bf2d5ae29b53994096d9e92c3713
Athinodoros <athinodoros.sgouromallis@hotmail.com>
2014-12-16 14:45:17 +0100

    implemented all abstract methods
    the player and the factory

---
+++ src/y10/Y10Factory.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package y10;
+
+import battleship.interfaces.BattleshipsPlayer;
+import tournament.player.PlayerFactory;
+
+/**
+ *
+ * @author Athinodwros
+ */
+public class Y10Factory implements PlayerFactory<BattleshipsPlayer>{
+
+    @Override
+    public BattleshipsPlayer getNewInstance() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getID() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
